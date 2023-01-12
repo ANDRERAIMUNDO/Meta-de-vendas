0ac2f2ac179be69fc050a49e67b97f5de6745279
ANDRERAIMUNDO <9000andre@gmail.com>
2023-01-12 18:32:05 -0300

    novo server

--- frontend/src/ultil/request.ts
+++ frontend/src/ultil/request.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "https://arct-meta.herokuapp.com"; 
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "https://demo----meta.buddy.cloud";
